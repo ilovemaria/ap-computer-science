d6ad47022d83ce891c68dbe481d39b0fd53aebcf
Jack  Hu <jackhumuchbusiness@gmail.com>
2022-11-02 09:55:23 -0700

    b

---
+++ nestedloops.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class nestedloops{
+
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+        for (y=0; y<5;y++) {
+            for (x=0;x<=y;x++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        } 
+        
+
+    }
+ 
+
+}
